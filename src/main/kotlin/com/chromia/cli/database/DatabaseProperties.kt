1b3e2287b47d81054d98785e4493fb587df7ec9e
carl.jernbacker <carl.jernbacker@chromaway.com>
2022-11-17 10:08:46 +0000

    Feature/test

---
+++ src/main/kotlin/com/chromia/cli/database/DatabaseProperties.kt
@@ -0,0 +1,14 @@
+package com.chromia.cli.compile
+
+import java.util.*
+
+class DatabaseProperties(properties: Properties) {
+    var host: String = properties["host"].toString()
+    var database: String = properties["database"].toString()
+    var username: String = properties["username"].toString()
+    var password: String = properties["password"].toString()
+
+    fun getConnectionUrl (): String {
+        return String.format("jdbc:postgresql://%s/%s?user=%s&password=%s", host, database, username, password)
+    }
+}
