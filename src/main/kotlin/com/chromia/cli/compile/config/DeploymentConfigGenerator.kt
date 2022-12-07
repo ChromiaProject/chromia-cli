359a25ddd2e6b0f7e4e59d54302a49a16b4ef4d6
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-07 14:41:14 +0100

    Remove dependency of deployment-tool package

--- src/main/kotlin/com/chromia/cli/compile/config/DeploymentConfigGenerator.kt
+++ src/main/kotlin/com/chromia/cli/compile/config/DeploymentConfigGenerator.kt
@@ -2,7 +2,6 @@ package com.chromia.cli.compile.config
 
 import com.chromia.cli.model.DeploymentModel
 import net.postchain.common.BlockchainRid
-import net.postchain.deployment.BlockchainConfiguration
 import net.postchain.gtv.Gtv
 import net.postchain.gtv.GtvEncoder
 import net.postchain.gtv.gtvml.GtvMLEncoder
