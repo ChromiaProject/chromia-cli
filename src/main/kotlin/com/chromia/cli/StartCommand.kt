da5d52bc1518ea56b2364a22e4deb6119044e1cf
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-14 17:38:44 +0100

    Make it possible to specify which blockchains to start

--- src/main/kotlin/com/chromia/cli/StartCommand.kt
+++ src/main/kotlin/com/chromia/cli/StartCommand.kt
@@ -35,6 +35,9 @@ class StartCommand : CliktCommand(help = "Starts a node") {
             .file(mustExist = true, canBeDir = false)
             .multiple()
 
+    private val blockchains by option(help = "Only start specified blockchains")
+            .multiple()
+
     private val nodeConfig by nodePropertiesOption().defaultLazy { getDefaultNodeConfig(settings) }
 
     init {
@@ -61,7 +64,9 @@ class StartCommand : CliktCommand(help = "Starts a node") {
                     .mapKeys { NamedBlockchainRid(it.key, BlockchainRid(PostchainUtils.calcBlockchainRid(it.value).toByteArray())) }
         }
 
-        configsToAdd.toList().forEachIndexed { index, (namedBlockchain, gtv) ->
+        val configsToStart = if (blockchains.isEmpty()) configsToAdd else configsToAdd.filter { blockchains.contains(it.key.name)  }
+
+        configsToStart.toList().forEachIndexed { index, (namedBlockchain, gtv) ->
             val gtvConfig = if (gtv["signers"] == null) {
                 gtv(*gtv.asDict().toList().toTypedArray(), "signers" to gtv(listOf(gtv(nodeConfig.pubKeyByteArray))))
             } else {
