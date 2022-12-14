51f77342c63509af67e088f4fbd280782f064fb4
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-14 10:48:50 +0100

    Support node discovery and client side load balancing when querying/tx to a deployment

--- src/main/kotlin/com/chromia/cli/util/deployTargetOptions.kt
+++ src/main/kotlin/com/chromia/cli/util/deployTargetOptions.kt
@@ -5,11 +5,16 @@ import com.github.ajalt.clikt.parameters.groups.OptionGroup
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import net.postchain.client.config.PostchainClientConfig
+import net.postchain.client.core.PostchainClient
+import net.postchain.client.impl.PostchainClientImpl
 import net.postchain.common.BlockchainRid
+import net.postchain.d1.client.ChromiaClientProvider
 
 sealed class DeploymentOption(name: String, help: String? = null) : OptionGroup(name, help) {
     abstract val brid: BlockchainRid
     abstract val url: String
+    abstract fun createClient(config: PostchainClientConfig): PostchainClient
 }
 
 class RemoteDeploymentOption(private val settings: () -> ChromiaCliModel) : DeploymentOption("Deployment", help = "Make query towards a configured deployment") {
@@ -21,7 +26,7 @@ class RemoteDeploymentOption(private val settings: () -> ChromiaCliModel) : Depl
             val deploymentModel = settings().deployments[name]
             require(deploymentModel != null) { "Deployment named $name not found in configuration" }
             val blockchainRid = deploymentModel.chains[blockchain]
-            require(blockchainRid != null ) { "Blockchain named $blockchain not found in deployment configuration"}
+            require(blockchainRid != null) { "Blockchain named $blockchain not found in deployment configuration" }
             return blockchainRid
         }
 
@@ -31,6 +36,10 @@ class RemoteDeploymentOption(private val settings: () -> ChromiaCliModel) : Depl
             require(deploymentModel != null) { "Deployment named $name not found in configuration" }
             return deploymentModel.apiUrl
         }
+
+    override fun createClient(config: PostchainClientConfig) = ChromiaClientProvider.fromClientConfig(
+            config.copy(blockchainRid = settings().deployments[name]!!.blockchainRid)
+    ).blockchain(config.blockchainRid)
 }
 
 class LocalDeploymentOption : DeploymentOption("Node", help = "Make query/tx towards a test node") {
@@ -42,4 +51,6 @@ class LocalDeploymentOption : DeploymentOption("Node", help = "Make query/tx tow
 
     override val url: String
         get() = apiUrl
+
+    override fun createClient(config: PostchainClientConfig) = PostchainClientImpl(config)
 }
