c654fa0a9f1cfe75be4badf3301cac90ed6bbfa2
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-15 09:59:43 +0100

    Rename blockchains to name option

--- src/main/kotlin/com/chromia/cli/StartCommand.kt
+++ src/main/kotlin/com/chromia/cli/StartCommand.kt
@@ -35,7 +35,7 @@ class StartCommand : CliktCommand(help = "Starts a node") {
             .file(mustExist = true, canBeDir = false)
             .multiple()
 
-    private val blockchains by option(help = "Only start specified blockchains")
+    private val name by option(help = "Only start specified blockchains (multiple)", metavar = "NAME")
             .multiple()
 
     private val nodeConfig by nodePropertiesOption().defaultLazy { getDefaultNodeConfig(settings) }
@@ -64,7 +64,7 @@ class StartCommand : CliktCommand(help = "Starts a node") {
                     .mapKeys { NamedBlockchainRid(it.key, BlockchainRid(PostchainUtils.calcBlockchainRid(it.value).toByteArray())) }
         }
 
-        val configsToStart = if (blockchains.isEmpty()) configsToAdd else configsToAdd.filter { blockchains.contains(it.key.name)  }
+        val configsToStart = if (name.isEmpty()) configsToAdd else configsToAdd.filter { name.contains(it.key.name)  }
 
         configsToStart.toList().forEachIndexed { index, (namedBlockchain, gtv) ->
             val gtvConfig = if (gtv["signers"] == null) {
