81aa1487592845db09627770ce58646628776f40
Tim Steinholtz <tim.steinholtz@netlight.com>
2023-01-19 16:39:48 +0100

    Update node command

--- src/main/kotlin/com/chromia/cli/StartCommand.kt
+++ src/main/kotlin/com/chromia/cli/StartCommand.kt
@@ -1,46 +1,21 @@
 package com.chromia.cli
 
-import com.chromia.cli.compile.NodeConfig.getDefaultNodeConfig
-import com.chromia.cli.compile.config.NamedBlockchainRid
-import com.chromia.cli.util.nodePropertiesOption
-import com.chromia.cli.util.settingsOption
 import com.chromia.cli.util.wipeDatabaseOption
-import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.output.CliktHelpFormatter
-import com.github.ajalt.clikt.parameters.options.defaultLazy
-import com.github.ajalt.clikt.parameters.options.multiple
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.file
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import mu.withLoggingContext
 import net.postchain.PostchainNode
 import net.postchain.api.internal.BlockchainApi
 import net.postchain.base.withReadWriteConnection
-import net.postchain.common.BlockchainRid
 import net.postchain.core.EContext
-import net.postchain.gtv.GtvDecoder
 import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.metrics.BLOCKCHAIN_RID_TAG
 import net.postchain.metrics.CHAIN_IID_TAG
 import net.postchain.metrics.NODE_PUBKEY_TAG
-import net.postchain.rell.utils.PostchainUtils
 
-class StartCommand : CliktCommand(help = "Starts a node") {
-    private val settings by settingsOption()
+class StartCommand : AbstractNodeCommand(help = "Starts a node") {
     private val wipe by wipeDatabaseOption()
-    private val blockchainConfigs by option("-bc", "--blockchain-config",
-            help = "Manually specify which blockchain-configs to run")
-            .file(mustExist = true, canBeDir = false)
-            .multiple()
 
-    private val name by option(help = "Only start specified blockchains (multiple)", metavar = "NAME")
-            .multiple()
-
-    private val nodeConfig by nodePropertiesOption().defaultLazy { getDefaultNodeConfig(settings.model) }
 
     init {
         context { helpFormatter = CliktHelpFormatter(showDefaultValues = true) }
@@ -52,28 +27,9 @@ class StartCommand : CliktCommand(help = "Starts a node") {
     private fun startPostchainNode() {
         val chainsToStart = mutableListOf<Long>()
         val node = PostchainNode(nodeConfig, wipeDb = wipe, debug = true)
-        val configsToAdd = if (blockchainConfigs.isEmpty()) {
-            BuildCommand.compile(settings)
-        } else {
-            blockchainConfigs
-                    .associate {
-                        if (it.extension == "gtv") {
-                            it.nameWithoutExtension to GtvDecoder.decodeGtv(it.inputStream())
-                        }else {
-                            it.nameWithoutExtension to PostchainUtils.xmlToGtv(it.readText())
-                        }
-                    }
-                    .mapKeys { NamedBlockchainRid(it.key, BlockchainRid(PostchainUtils.calcBlockchainRid(it.value).toByteArray())) }
-        }
 
-        val configsToStart = if (name.isEmpty()) configsToAdd else configsToAdd.filter { name.contains(it.key.name)  }
-
-        configsToStart.toList().forEachIndexed { index, (namedBlockchain, gtv) ->
-            val gtvConfig = if (gtv["signers"] == null) {
-                gtv(*gtv.asDict().toList().toTypedArray(), "signers" to gtv(listOf(gtv(nodeConfig.pubKeyByteArray))))
-            } else {
-                gtv
-            }
+        extractConfigs().toList().forEachIndexed { index, (namedBlockchain, gtv) ->
+            val gtvWithSigners = addSigners(gtv)
             val iid = index.toLong()
             chainsToStart.add(iid)
             withLoggingContext(
@@ -82,10 +38,10 @@ class StartCommand : CliktCommand(help = "Starts a node") {
                     BLOCKCHAIN_RID_TAG to namedBlockchain.blockchainRid.toHex()
             ) {
                 withReadWriteConnection(node.postchainContext.storage, iid) { eContext: EContext ->
-                    BlockchainApi.initializeBlockchain(eContext, namedBlockchain.blockchainRid, override = true, gtvConfig)
+                    BlockchainApi.initializeBlockchain(eContext, namedBlockchain.blockchainRid, override = true, gtvWithSigners)
                     val lastHeight = BlockchainApi.getLastBlockHeight(eContext)
                     if (lastHeight > 0) {
-                        BlockchainApi.addConfiguration(eContext, lastHeight + 1 , override = true, gtvConfig)
+                        BlockchainApi.addConfiguration(eContext, lastHeight + 1 , override = true, gtvWithSigners)
                     }
                 }
             }
---
+++ src/main/kotlin/com/chromia/cli/UpdateCommand.kt
@@ -0,0 +1,21 @@
+package com.chromia.cli
+
+import net.postchain.StorageBuilder
+import net.postchain.api.internal.BlockchainApi
+import net.postchain.base.withReadWriteConnection
+import net.postchain.core.EContext
+
+class UpdateCommand : AbstractNodeCommand(help = "Updates a node") {
+    override fun run() {
+        val storage = StorageBuilder.buildStorage(nodeConfig, false)
+
+        extractConfigs().toList().forEachIndexed { index, (_, gtv) ->
+            val gtvWithSigners = addSigners(gtv)
+            withReadWriteConnection(storage, index.toLong()) { eContext: EContext ->
+                val lastHeight = BlockchainApi.getLastBlockHeight(eContext)
+                require(lastHeight > 0) { "Blockchain must be initialized before you can update it" }
+                BlockchainApi.addConfiguration(eContext, lastHeight + 5, override = true, gtvWithSigners)
+            }
+        }
+    }
+}
