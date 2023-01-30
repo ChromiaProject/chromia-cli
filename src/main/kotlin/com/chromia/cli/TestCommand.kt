f5cb881066899107948ab793f06d46fe13a655b9
Joar Svartholm <joar.svartholm@chromaway.com>
2023-01-30 16:08:45 +0100

    Make sure module args are passed to rell properly

--- src/main/kotlin/com/chromia/cli/TestCommand.kt
+++ src/main/kotlin/com/chromia/cli/TestCommand.kt
@@ -2,7 +2,6 @@ package com.chromia.cli
 
 import com.chromia.cli.compile.ContextCreator
 import com.chromia.cli.database.DatabaseUtil
-import com.chromia.cli.parser.ModuleArgs.getModuleArgsValues
 import com.chromia.cli.util.modulesOption
 import com.chromia.cli.util.settingsOption
 import com.github.ajalt.clikt.core.CliktCommand
@@ -11,21 +10,25 @@ import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import net.postchain.common.BlockchainRid
+import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.rell.compiler.base.core.C_CompilerModuleSelection
 import net.postchain.rell.compiler.base.core.C_CompilerOptions
 import net.postchain.rell.compiler.base.utils.C_SourceDir
-import net.postchain.rell.lib.test.Rt_DynamicBlockRunnerStrategy
+import net.postchain.rell.lib.test.UnitTestBlockRunner
 import net.postchain.rell.model.R_App
 import net.postchain.rell.model.R_FunctionDefinition
 import net.postchain.rell.model.R_ModuleName
+import net.postchain.rell.runtime.Rt_BlockRunnerStrategy
 import net.postchain.rell.runtime.Rt_ChainSqlMapping
 import net.postchain.rell.runtime.Rt_Exception
 import net.postchain.rell.runtime.utils.Rt_Utils
 import net.postchain.rell.sql.NoConnSqlManager
 import net.postchain.rell.sql.SqlManager
+import net.postchain.rell.tools.runcfg.RunConfigGtvBuilder
 import net.postchain.rell.utils.*
 
-class TestCommand: CliktCommand(help= "Run tests in working directory") {
+class TestCommand : CliktCommand(help = "Run tests in working directory") {
     private val modules by modulesOption()
     private val settings by settingsOption()
     private val tests by option(help = "test method pattern")
@@ -35,12 +38,14 @@ class TestCommand: CliktCommand(help= "Run tests in working directory") {
     init {
         context { helpFormatter = CliktHelpFormatter(showDefaultValues = true) }
     }
+
     override fun run() {
         runMultiModuleTest()
     }
+
     private fun runMultiModuleTest() {
 
-        val testModules = modules ?: settings.test.modules.map {  R_ModuleName.Companion.of(it) }
+        val testModules = modules ?: settings.test.modules.map { R_ModuleName.Companion.of(it) }
         val sourceDir = C_SourceDir.diskDir(sourceDir)
         val modSel = C_CompilerModuleSelection(testModules)
         val app = RellCliUtils.compileApp(sourceDir, modSel, settings.model.compilerQuiet, C_CompilerOptions.DEFAULT)
@@ -57,14 +62,15 @@ class TestCommand: CliktCommand(help= "Run tests in working directory") {
             runner(NoConnSqlManager, app, fns, sourceDir)
         }
     }
+
     private fun runner(sqlManager: SqlManager, app: R_App, fns: List<R_FunctionDefinition>, sourceDir: C_SourceDir) {
         val context = ContextCreator.createTestContext(app,
                 settings.model.compilerOptions,
                 BlockchainRid(ByteArray(32)),
                 Rt_ChainSqlMapping(100),
-                getModuleArgsValues(app, settings.test.moduleArgs))
+                settings.test.moduleArgs)
         val blockRunnerModules = app.modules.filter { !it.test && !it.abstract && !it.external }.map { it.name }
-        val blockRunnerStrategy = Rt_DynamicBlockRunnerStrategy(sourceDir, blockRunnerModules, context.keyPair)
+        val blockRunnerStrategy = TestBlockRunnerStrategy(sourceDir, blockRunnerModules, settings.test.moduleArgs)
         val testCtx = TestRunnerContext(context.sqlCtx, sqlManager, context.globalCtx, context.chainCtx, blockRunnerStrategy, app)
         val cases = fns.map { TestRunnerCase(null, it) }
         val results = TestRunnerResults()
@@ -119,8 +125,33 @@ class TestCommand: CliktCommand(help= "Run tests in working directory") {
                 val msg = Rt_Utils.appendStackTrace("Error: ${e.message}", e.info.stack)
                 echo(msg)
             }
+
             else -> {
                 echo(e.stackTraceToString())
+            }
+        }
+    }
+
+    private inner class TestBlockRunnerStrategy(val sourceDir: C_SourceDir, val modules: List<R_ModuleName>, val moduleArgs: Map<String, Map<String, Gtv>>) : Rt_BlockRunnerStrategy() {
+
+        override fun createGtvConfig(): Gtv {
+            return UnitTestBlockRunner.makeGtvConfig(BlockRunnerRellCliEnv(), sourceDir, modules, getKeyPair().pub).let { config ->
+                RunConfigGtvBuilder().apply {
+                    update(config)
+                    update(gtv(moduleArgs.mapValues { gtv(it.value) }), "gtx", "rell", "moduleArgs")
+                }.build()
+            }
+        }
+
+        override fun getKeyPair() = UnitTestBlockRunner.getTestKeyPair()
+
+        private inner class BlockRunnerRellCliEnv : RellCliEnv() {
+            override fun print(msg: String, err: Boolean) {
+                echo(msg)
+            }
+
+            override fun exit(status: Int): Nothing {
+                throw Rt_Exception.common("block_runner", "Gtv config generation failed")
             }
         }
     }
--- src/main/kotlin/com/chromia/cli/compile/ContextCreator.kt
+++ src/main/kotlin/com/chromia/cli/compile/ContextCreator.kt
@@ -1,52 +1,37 @@
 package com.chromia.cli.compile
 
+import com.chromia.cli.parser.ModuleArgs.getModuleArgsValues
 import net.postchain.common.BlockchainRid
 import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.gtv.GtvNull
 import net.postchain.rell.compiler.base.core.C_CompilerOptions
 import net.postchain.rell.lib.test.UnitTestBlockRunner
 import net.postchain.rell.model.R_App
-import net.postchain.rell.model.R_ModuleName
 import net.postchain.rell.module.RellPostchainModuleEnvironment
 import net.postchain.rell.runtime.*
-import net.postchain.rell.utils.BytesKeyPair
-import net.postchain.rell.utils.RellCliUtils
-import net.postchain.rell.utils.immMapOf
 
 object ContextCreator {
     data class Context(
             var globalCtx: Rt_GlobalContext,
             var chainCtx: Rt_ChainContext,
-            val sqlCtx: Rt_SqlContext,
-            val keyPair: BytesKeyPair
+            val sqlCtx: Rt_SqlContext
     )
+
     fun createTestContext(
-        app: R_App,
-        coProperties: C_CompilerOptions,
-        blockchainRid: BlockchainRid,
-        sqlMapper: Rt_ChainSqlMapping,
-        moduleArgs: Map<R_ModuleName, Rt_Value>
+            app: R_App,
+            coProperties: C_CompilerOptions,
+            blockchainRid: BlockchainRid,
+            sqlMapper: Rt_ChainSqlMapping,
+            moduleArgs: Map<String, Map<String, Gtv>>
     ): Context {
         val globalCtx = createGlobalContext(coProperties)
-        val chainCtx = Rt_ChainContext(GtvNull, moduleArgs, blockchainRid)
+        val chainCtx = Rt_ChainContext(GtvNull, getModuleArgsValues(app, moduleArgs), blockchainRid)
         val sqlCtx = Rt_RegularSqlContext.createNoExternalChains(app, sqlMapper)
-        val keyPair = UnitTestBlockRunner.getTestKeyPair() //this is static in the test scope Bob and alice
-        return Context(globalCtx, chainCtx, sqlCtx, keyPair)
+        return Context(globalCtx, chainCtx, sqlCtx)
     }
 
     fun createGlobalContext(compilerOptions: C_CompilerOptions): Rt_GlobalContext {
         return Rt_GlobalContext(compilerOptions, Rt_OutPrinter, Rt_OutPrinter, RellPostchainModuleEnvironment.DEFAULT)
-    }
-
-    fun createRegularAppContext(globalCtx: Rt_GlobalContext, app: R_App): Rt_AppContext {
-        return Rt_AppContext(
-                globalCtx,
-                RellCliUtils.createChainContext(),
-                app,
-                repl = false,
-                test = false,
-                replOut = null,
-                blockRunnerStrategy = Rt_UnsupportedBlockRunnerStrategy
-        )
     }
 }
