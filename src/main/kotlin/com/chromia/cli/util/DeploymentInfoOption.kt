6b1b59ad7d883d08e897ff95fc3000e56ea95168
caje <caje@netlight.com>
2023-03-28 11:38:43 +0200

    fixes pr comments

--- src/main/kotlin/com/chromia/cli/util/DeploymentInfoOption.kt
+++ src/main/kotlin/com/chromia/cli/util/DeploymentInfoOption.kt
@@ -16,40 +16,26 @@ sealed class DeploymentInfoOption(name: String, help: String? = null) : OptionGr
 }
 
 class ConfiguredDeploymentInfoOption(private val settings: () -> ChromiaCliModel) : DeploymentInfoOption("Configured", help = "Information about a deployed blockchain") {
-    private val target by option("--target", help = "If a specific target deploy model should be used").required().validate {
+    private val target by option("--network", help = "If a specific target deploy model should be used").required().validate {
         val deployment = settings().deployments[it]
         require(deployment != null) { "Deployment $it not found" }
     }
-    private val blockchain by option(help = "Name of blockchain to deploy").required().validate {
-        require(settings().deployments[target]!!.chains[it] != null) { "Blockchain $it not found" }
-    }
+    private val blockchain by option(help = "Name of blockchain to deploy").required()
+            .validate {
+                require(settings().deployments[target]!!.chains[it] != null) { "Blockchain $it not found" }
+            }
 
     private val network: DeploymentModel get() = settings().deployments[target]!!
-
-    override val brid: BlockchainRid
-        get() {
-            return network.chains[blockchain]!!
-        }
-
-    override val blockchainName: String
-        get() = blockchain
-
-    override val urls: List<String>
-        get() {
-            return network.urls
-        }
+    override val brid: BlockchainRid get() = network.chains[blockchain]!!
+    override val blockchainName: String get() = blockchain
+    override val urls: List<String> get() = network.urls
 }
 
 class ManualDeploymentInfoOption : DeploymentInfoOption("Manual", help = "Information about a deployed blockchain that is not in the settings file") {
     private val blockchainRid by option("-brid", "--blockchain-rid", help = "Target Blockchain RID").required()
     private val url by option(help = "Target url").multiple()
 
-    override val urls: List<String>
-        get() = url
-
-    override val brid: BlockchainRid
-        get() = blockchainRid.let { BlockchainRid.buildFromHex(it) }
-
-    override val blockchainName: String
-        get() = blockchainRid
+    override val urls: List<String> get() = url
+    override val brid: BlockchainRid get() = blockchainRid.let { BlockchainRid.buildFromHex(it) }
+    override val blockchainName: String get() = blockchainRid
 }
