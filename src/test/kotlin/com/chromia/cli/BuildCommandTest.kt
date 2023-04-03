fe7c5ae7401e4e130515f18dfdb68f17f3e21cd8
Joar Svartholm <joar.svartholm@chromaway.com>
2023-04-03 10:02:03 +0200

    Whilelist anchoring and icmf/iccf modules when validating configuration

--- src/test/kotlin/com/chromia/cli/BuildCommandTest.kt
+++ src/test/kotlin/com/chromia/cli/BuildCommandTest.kt
@@ -1,11 +1,13 @@
 package com.chromia.cli
 
 import assertk.assert
+import assertk.assertions.containsAll
 import com.chromia.cli.util.CommandExtension
 import com.chromia.cli.util.TestConsole
 import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.core.FileNotFound
 import com.github.ajalt.clikt.core.context
+import net.postchain.rell.utils.PostchainUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -69,5 +71,36 @@ internal class BuildCommandTest {
         """.trimIndent())
         val e = assertFailsWith<CliktError> { command.parse() }
         assertTrue(e.message!!.contains("Module initialization failed: Decoding type 'integer': expected INTEGER, actual STRING"))
+    }
+
+    @Test
+    fun whiteListedGtxModules() {
+        File(dir, "config.yml").writeText("""
+            blockchains:
+              hello:
+                module: main
+                config:
+                  gtx:
+                    modules:
+                      - "net.postchain.d1.anchoring.system.SystemAnchoringGTXModule"
+                      - "net.postchain.d1.anchoring.cluster.ClusterAnchoringGTXModule"
+                      - "net.postchain.d1.icmf.IcmfSenderGTXModule"
+                      - "net.postchain.d1.icmf.IcmfReceiverGTXModule"
+                      - "net.postchain.d1.iccf.IccfGTXModule"
+        """.trimIndent())
+        File(dir, "src/main.rell").writeText("""
+            module;
+        """.trimIndent())
+        command.parse()
+        val outputFile = File(dir, "build/hello.xml")
+        assertTrue(outputFile.exists())
+        val outputGtv = PostchainUtils.xmlToGtv(outputFile.readText())
+        assert(outputGtv["gtx"]?.get("modules")?.asArray()?.map { it.asString() }!!).containsAll(
+                "net.postchain.d1.anchoring.system.SystemAnchoringGTXModule",
+                "net.postchain.d1.anchoring.cluster.ClusterAnchoringGTXModule",
+                "net.postchain.d1.icmf.IcmfSenderGTXModule",
+                "net.postchain.d1.icmf.IcmfReceiverGTXModule",
+                "net.postchain.d1.iccf.IccfGTXModule"
+        )
     }
 }
