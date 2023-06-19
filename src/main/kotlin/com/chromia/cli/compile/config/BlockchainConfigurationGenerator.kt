1f10a9306a8971020ed48c495c5db9c91abf721c
Robert Wideberg <robert.wideberg@chromaway.com>
2023-06-19 13:18:50 +0200

    Better default for fast revolt

--- src/main/kotlin/com/chromia/cli/compile/config/BlockchainConfigurationGenerator.kt
+++ src/main/kotlin/com/chromia/cli/compile/config/BlockchainConfigurationGenerator.kt
@@ -96,7 +96,7 @@ class BlockchainConfigurationGenerator(
         b.update(gtv("name" to gtv(BaseBlockBuildingStrategy::class.qualifiedName!!)), "blockstrategy")
         b.update(gtv(GTXBlockchainConfigurationFactory::class.qualifiedName!!), "configurationfactory")
         b.update(gtv("HEADER_HASH"), "config_consensus_strategy")
-        b.update(gtv(1000), "revolt", "fast_revolt_status_timeout")
+        b.update(gtv(2000), "revolt", "fast_revolt_status_timeout")
 
         val modulesGtv: MutableList<Gtv> = mutableListOf(
                 gtv(RellPostchainModuleFactory::class.qualifiedName!!),
