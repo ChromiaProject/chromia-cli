9d6f57222fa86aae83a7485db889e949214330b1
Joar Svartholm <joar.svartholm@chromaway.com>
2023-09-07 16:48:00 +0200

    Initial ft auth transactions

--- chromia-cli/src/main/kotlin/com/chromia/cli/TxCommand.kt
+++ chromia-cli/src/main/kotlin/com/chromia/cli/TxCommand.kt
@@ -6,6 +6,7 @@ import com.chromia.cli.util.LocalDeploymentOption
 import com.chromia.cli.util.RemoteDeploymentOption
 import com.chromia.cli.util.secretOption
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.PrintMessage
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.arguments.transformAll
@@ -13,6 +14,10 @@ import com.github.ajalt.clikt.parameters.groups.cooccurring
 import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import net.postchain.common.types.WrappedByteArray
+import net.postchain.gtv.Gtv
+import net.postchain.gtv.GtvFactory.gtv
+import net.postchain.gtv.mapper.Name
 import net.postchain.gtv.parse.GtvParser
 
 
@@ -24,6 +29,7 @@ class TxCommand : CliktCommand(help = "Make a transaction") {
     private val deploymentTarget by RemoteDeploymentOption { settings.model ?: ChromiaModel() }.cooccurring()
     private val awaitConfirmation by option("--await", "-a", help = "Wait for transaction to be included in a block").flag()
     private val nop by option("-nop", help = "Adds a nop to the transaction").flag()
+    private val ftAuth by option().flag()
 
     private val opName by argument(help = "name of the operation to execute.")
 
@@ -39,10 +45,32 @@ class TxCommand : CliktCommand(help = "Make a transaction") {
                 args.map { GtvParser.parse(it) }
             }
 
+    data class AuthDescriptor(
+            @Name("id") val id: WrappedByteArray,
+            @Name("args") val args: List<Gtv>,
+            @Name("created") val created: Long,
+            @Name("auth_type") val authType: String, // Enum
+            @Name("rules") val rules: Gtv // Can be GtvNull
+    )
+
     override fun run() {
         val target = deploymentTarget ?: explicitTarget
-        val res = target.createClient(settings.config.get(target.url, target.brid, secret))
-                .transactionBuilder()
+        val postchainClientConfig = settings.config.get(target.url, target.brid, secret)
+        val client = target.createClient(postchainClientConfig)
+        val transactionBuilder = client.transactionBuilder()
+        if (ftAuth) {
+            val signer = postchainClientConfig.signers.singleOrNull() ?: throw PrintMessage("A single keypair is required to use ft authentication")
+            val accountId = client.query("ft4.get_accounts_by_participant_id", gtv(mapOf("id" to gtv(signer.pubKey.data)))).asArray()
+            echo(accountId.map { it.toString() })
+            if (accountId.isEmpty()) throw PrintMessage("No accounts found for pubkey ${signer.pubKey}")
+            val authDescriptors = client.query("ft4.get_account_auth_descriptors_by_participant_id", gtv(mapOf("account_id" to accountId.first(), "participant_id" to gtv(signer.pubKey.data)))).asArray()
+            echo(authDescriptors.map { it.asDict() })
+            val flags = client.query("ft4.get_auth_flags", gtv(mapOf("op_name" to gtv(opName)))).asArray().map { it.asString() }
+            val matchingDescriptor = authDescriptors.map { it.asDict() }.first { flags.any { flag -> it["args"]!!.asArray().first().asArray().map { it.asString() }.contains(flag) }}
+            echo(flags)
+            transactionBuilder.addOperation("ft4.ft_auth", accountId.first(), matchingDescriptor["id"]!!)
+        }
+        val res = transactionBuilder
                 .addOperation(opName, *args.toTypedArray())
                 .run {
                     if (nop) addNop()
