14a72087b00a70ba6e47de8e1bf8b2855aec7885
Tim Steinholtz <tim.steinholtz@chromaway.com>
2023-09-20 14:45:17 +0000

    Private repository ssh support

--- chromia-build-tools/src/main/kotlin/com/chromia/build/tools/lib/GitRepositoryCloner.kt
+++ chromia-build-tools/src/main/kotlin/com/chromia/build/tools/lib/GitRepositoryCloner.kt
@@ -2,12 +2,19 @@ package com.chromia.build.tools.lib
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.errors.InvalidRemoteException
+import org.eclipse.jgit.internal.transport.sshd.agent.connector.Factory
 import org.eclipse.jgit.lib.TextProgressMonitor
+import org.eclipse.jgit.transport.SshSessionFactory
+import org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder
+import org.eclipse.jgit.util.FS
 import java.io.File
 
-class GitRepositoryCloner : RepositoryCloner {
+class GitRepositoryCloner(val sshDir: File? = null) : RepositoryCloner {
+
     override fun clone(registry: String, target: File, tagOrBranch: String?) {
         try {
+            val sshdSessionFactory = createSshdSessionFactory()
+            SshSessionFactory.setInstance(sshdSessionFactory)
             Git.cloneRepository()
                     .apply { if (tagOrBranch != null) setBranch(tagOrBranch) }
                     .setDirectory(target)
@@ -20,4 +27,16 @@ class GitRepositoryCloner : RepositoryCloner {
             throw LibraryInstallException(e.message!!)
         }
     }
+
+    private fun createSshdSessionFactory() = SshdSessionFactoryBuilder()
+            .setConnectorFactory(Factory())
+            .apply {
+                if (sshDir != null) {
+                    setSshDirectory(sshDir)
+                } else {
+                    setSshDirectory(File(FS.DETECTED.userHome(), ".ssh"))
+                }
+            }
+            .setHomeDirectory(FS.DETECTED.userHome())
+            .build(null)
 }
