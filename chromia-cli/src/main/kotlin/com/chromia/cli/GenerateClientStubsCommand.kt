658e95a1cb734f34ab7b50757d11e0b983c6b755
Joar Svartholm <joar.svartholm@chromaway.com>
2023-10-20 17:25:55 +0200

    Remove LanguageSupport enum and revert how we create documentfactory

--- chromia-cli/src/main/kotlin/com/chromia/cli/GenerateClientStubsCommand.kt
+++ chromia-cli/src/main/kotlin/com/chromia/cli/GenerateClientStubsCommand.kt
@@ -2,7 +2,6 @@ package com.chromia.cli
 
 import com.chromia.cli.tools.config.chromiaModelOption
 import com.chromia.cli.tools.env.CliktCliEnv
-import com.chromia.cli.util.LanguageSupport
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
 import com.github.ajalt.clikt.parameters.groups.groupSwitch
@@ -13,6 +12,7 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.split
 import com.github.ajalt.clikt.parameters.types.file
+import java.io.File
 import net.postchain.rell.codegen.CodeGenerator
 import net.postchain.rell.codegen.CodeGeneratorConfig
 import net.postchain.rell.codegen.MermaidCodeGeneratorConfig
@@ -25,7 +25,6 @@ import net.postchain.rell.codegen.kotlin.KotlinCodeGeneratorConfig
 import net.postchain.rell.codegen.kotlin.KotlinDocumentFactory
 import net.postchain.rell.codegen.typescript.TypescriptCodeGeneratorConfig
 import net.postchain.rell.codegen.typescript.TypescriptDocumentFactory
-import java.io.File
 
 class GenerateClientStubsCommand : CliktCommand(name = "generate-client-stubs", help = "Generates client code for a rell dapp") {
     private val settings by chromiaModelOption()
@@ -44,13 +43,7 @@ class GenerateClientStubsCommand : CliktCommand(name = "generate-client-stubs", 
             .file(canBeFile = false)
 
     override fun run() {
-        val factory = when (language) {
-            is KotlinOption -> KotlinDocumentFactory(language as KotlinCodeGeneratorConfig)
-            is JavascriptOption -> JavascriptDocumentFactory()
-            is TypescriptOption -> TypescriptDocumentFactory()
-            is MermaidOption -> MermaidDocumentFactory(language as MermaidCodeGeneratorConfig)
-        }
-        val generator = CodeGenerator(factory, language)
+        val generator = CodeGenerator(language.factory(), language, CliktCliEnv(this))
         val modules = moduleName ?: settings.model.blockchains.map { it.value.module }
         val sections = modules.flatMap { generator.createSections(settings.sourceDir, listOf(it)) }
         val documents = generator.constructDocuments(sections)
@@ -60,21 +53,29 @@ class GenerateClientStubsCommand : CliktCommand(name = "generate-client-stubs", 
     }
 }
 
-sealed class LanguageOption(language: String) : CodeGeneratorConfig, OptionGroup(language) {
+sealed class LanguageOption(language: String) : CodeGeneratorConfig, OptionGroup(
+        name = language,
+        help = "Options for language ${language.lowercase()}"
+) {
+    abstract fun factory(): DocumentFactory
 }
 
-class KotlinOption : KotlinCodeGeneratorConfig, LanguageOption(LanguageSupport.Kotlin.name) {
+class KotlinOption : KotlinCodeGeneratorConfig, LanguageOption("Kotlin") {
     private val packageName by option("--package", help = "Name of package").required()
     override fun packageName() = packageName
+    override fun factory() = KotlinDocumentFactory(this)
 }
 
 class MermaidOption : MermaidCodeGeneratorConfig, LanguageOption("Mermaid") {
     private val mdx by option(help = "Surround with mdx tags").flag()
     override fun mdx() = mdx
+    override fun factory() = MermaidDocumentFactory(this)
 }
 
-class TypescriptOption : TypescriptCodeGeneratorConfig, LanguageOption(LanguageSupport.Typescript.name) {
+class TypescriptOption : TypescriptCodeGeneratorConfig, LanguageOption("Typescript") {
+    override fun factory() = TypescriptDocumentFactory()
 }
 
-class JavascriptOption : JavascriptCodeGeneratorConfig, LanguageOption(LanguageSupport.Javascript.name) {
+class JavascriptOption : JavascriptCodeGeneratorConfig, LanguageOption("Javascript") {
+    override fun factory() = JavascriptDocumentFactory()
 }
