dadadecb4ae75100f31041fff9ced8247ca7af4b
Tim Steinholtz <tim.steinholtz@chromaway.com>
2023-12-21 14:44:20 +0100

    Updates keygen to use bip

--- chromia-cli/src/main/kotlin/com/chromia/cli/KeygenCommand.kt
+++ chromia-cli/src/main/kotlin/com/chromia/cli/KeygenCommand.kt
@@ -2,6 +2,7 @@ package com.chromia.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import net.postchain.common.toHex
@@ -13,7 +14,7 @@ import net.postchain.crypto.secp256k1_derivePubKey
 import org.bitcoinj.crypto.MnemonicCode
 import java.io.File
 import java.io.FileOutputStream
-import java.util.*
+import java.util.Properties
 
 class KeygenCommand : CliktCommand(name = "keygen", help = "Generates public/private key pair") {
 
@@ -29,11 +30,14 @@ class KeygenCommand : CliktCommand(name = "keygen", help = "Generates public/pri
     private val file by option("-s", "--save", help = "File to save the generated keypair in")
             .file(canBeDir = false)
 
+    private val deprecated by option("-dr", "--deprecated-recovery", help = "Used to recover keys from Mnemonic generated before version 0.13.2, will be removed in the future")
+            .flag()
+
     /**
      * Cryptographic key generator. Will generate a pair of public and private keys and print to stdout.
      */
     override fun run() {
-        val (keyPair, mnemonic) = generateSecp256k1KeyPairWithMnemonic(wordList)
+        val (keyPair, mnemonic) = generateSecp256k1KeyPairWithMnemonic(wordList, deprecated)
 
         file?.let {
             saveSecp256k1KeyPair(keyPair, it.absoluteFile)
@@ -48,23 +52,29 @@ class KeygenCommand : CliktCommand(name = "keygen", help = "Generates public/pri
     }
 }
 
-private fun generateSecp256k1KeyPairWithMnemonic(wordList: String): Pair<KeyPair, String> {
+private fun generateSecp256k1KeyPairWithMnemonic(wordList: String, deprecated: Boolean = false): Pair<KeyPair, String> {
     val cs = Secp256K1CryptoSystem()
 
-    var privKey = cs.generatePrivKey().data
-    val mnemonicInstance = MnemonicCode.INSTANCE
-    var mnemonic = mnemonicInstance.toMnemonic(privKey).joinToString(" ")
-    if (wordList.isNotEmpty()) {
-        val words = wordList.split(" ")
-        mnemonicInstance.check(words)
-        mnemonic = wordList
-        privKey = mnemonicInstance.toEntropy(words)
+    if (deprecated) {
+        check(wordList.isNotEmpty()) { "Mnemonic is needed to use --deprecated-recovery" }
     }
 
-    val pubKey = secp256k1_derivePubKey(privKey)
+    // Recover the mnemonic from old Secp256K1CryptoSystem were the mnemonic was not bip39 compatible
+    if (wordList.isNotEmpty() && deprecated) {
+        val words = wordList.split(" ")
+        val mnemonicInstance = MnemonicCode.INSTANCE
+        mnemonicInstance.check(words)
+        val privKey = mnemonicInstance.toEntropy(words)
+        val pubKey = secp256k1_derivePubKey(privKey)
+        val keyPair = KeyPair(PubKey(pubKey), PrivKey(privKey))
+        return keyPair to wordList
 
-    val keyPair = KeyPair(PubKey(pubKey), PrivKey(privKey))
-    return keyPair to mnemonic
+    } else if (wordList.isNotEmpty() && !deprecated) {
+        // New Secp256K1CryptoSystem were the mnemonic is bip39 compatible
+        return cs.recoverKeyPairFromMnemonic(wordList)
+    }
+
+    return cs.generateKeyPairWithMnemonic()
 }
 
 private fun saveSecp256k1KeyPair(keyPair: KeyPair, file: File) {
