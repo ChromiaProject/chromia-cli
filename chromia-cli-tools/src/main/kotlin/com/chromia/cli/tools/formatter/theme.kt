820b7242351794c1f879b09158dd80cdde3473ca
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-06-03 08:22:08 +0000

    Remove unused theme

--- chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/formatter/theme.kt
+++ chromia-cli-tools/src/main/kotlin/com/chromia/cli/tools/formatter/theme.kt
@@ -9,33 +9,21 @@ import com.github.ajalt.mordant.rendering.Theme
 import com.github.ajalt.mordant.table.TableBuilder
 import com.github.ajalt.mordant.table.table
 
-val theme = Theme {
-    // Use ANSI-16 codes for help colors
-    styles["success"] = TextColors.green
-    styles["info"] = TextColors.brightBlue
-    styles["warning"] = TextColors.brightYellow
-    styles["danger"] = TextColors.red
-    styles["muted"] = TextColors.gray
-
-    // Remove the border around code blocks
-    flags["markdown.code.block.border"] = false
-}
-
 val chromiaTheme = Theme {
     styles["success"] = TextColors.green
-    styles["info"] = TextColors.rgb("#CB92F0")
-    styles["warning"] = TextColors.rgb("#FFB600")
     styles["danger"] = TextColors.rgb("#FF405E")
+    styles["warning"] = TextColors.rgb("#FFB600")
+    styles["info"] = TextColors.rgb("#CB92F0")
     styles["muted"] = TextColors.rgb("#827382")
 
     // Remove the border around code blocks
     flags["markdown.code.block.border"] = false
 }
 
-val CliktCommand.danger get() = currentContext.terminal.theme.danger
 val CliktCommand.success get() = currentContext.terminal.theme.success
-val CliktCommand.info get() = currentContext.terminal.theme.info
+val CliktCommand.danger get() = currentContext.terminal.theme.danger
 val CliktCommand.warning get() = currentContext.terminal.theme.warning
+val CliktCommand.info get() = currentContext.terminal.theme.info
 val CliktCommand.muted get() = currentContext.terminal.theme.muted
 
 
