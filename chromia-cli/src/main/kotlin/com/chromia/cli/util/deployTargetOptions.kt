9efb1e46c6d8f939447f0dee9bebc8729002ecf3
Issame Zguiri <issame.zguiri@chromaway.com>
2025-03-20 11:54:14 +0100

    add handling when api-url is localhost

--- chromia-cli/src/main/kotlin/com/chromia/cli/util/deployTargetOptions.kt
+++ chromia-cli/src/main/kotlin/com/chromia/cli/util/deployTargetOptions.kt
@@ -38,7 +38,7 @@ sealed class DeploymentOption(name: String, help: String? = null) : OptionGroup(
         config: ChromiaClientConfig,
         d1BridFromModel: BlockchainRid
     ): PostchainClient {
-        if (d1BridFromModel != BlockchainRid.ZERO_RID) {
+        if (isLocalDeployment() || d1BridFromModel != BlockchainRid.ZERO_RID) {
             return config.setBrid(d1BridFromModel).client(PostchainClientProviderImpl())
         }
         val d1BridFetched = fetchBridFromChainID()
@@ -52,6 +52,8 @@ sealed class DeploymentOption(name: String, help: String? = null) : OptionGroup(
         // TODO: make BridFetcher takes multiple URLS
         return BridFetcher(httpHandler, urls.first()).fetchBlockchainRid(cid)
     }
+
+    private fun isLocalDeployment() = urls.size == 1 && urls.first().startsWith("http://localhost")
 }
 
 class RemoteDeploymentOption(private val settings: () -> ChromiaModel) : DeploymentOption("Deployment", help = "Use a configured deployment") {
