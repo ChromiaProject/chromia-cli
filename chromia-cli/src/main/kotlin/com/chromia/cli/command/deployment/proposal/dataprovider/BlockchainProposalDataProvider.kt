bcaf7c23f215eec5eccb59d820d7b4398ba60c71
Johan Nilsson <johan.nilsson@chromaway.com>
2025-05-09 16:39:45 +0200

    Bug fix in bc config merkel hash

--- chromia-cli/src/main/kotlin/com/chromia/cli/command/deployment/proposal/dataprovider/BlockchainProposalDataProvider.kt
+++ chromia-cli/src/main/kotlin/com/chromia/cli/command/deployment/proposal/dataprovider/BlockchainProposalDataProvider.kt
@@ -6,15 +6,12 @@ import com.github.ajalt.clikt.core.CliktCommand
 import net.postchain.base.configuration.BlockchainConfigurationData
 import net.postchain.client.core.PostchainClient
 import net.postchain.common.types.RowId
-import net.postchain.common.types.WrappedByteArray
-import net.postchain.common.wrap
+import net.postchain.gtv.GtvDecoder.decodeGtv
 
 class BlockchainProposalDataProvider : ProposalDataProvider<GetBlockchainProposalResult> {
-    private fun getDataHash(configData: WrappedByteArray) =
-            BlockchainConfigurationData.fromRaw(configData.data).configHash.wrap()
 
     override fun formatData(data: GetBlockchainProposalResult, cliktCommand: CliktCommand): Any {
-        return "Container: ${data.container}\nConfig hash: ${getDataHash(data.data)}"
+        return "Container: ${data.container}\nConfig hash: ${BlockchainConfigurationData.merkleHash(decodeGtv(data.data.data))}"
     }
 
     override fun getData(client: PostchainClient, proposalId: RowId): GetBlockchainProposalResult? {
