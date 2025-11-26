39e20da59a74966f7e9c06ace28a5e92b2cf0eb6
Issame Zguiri <issame.zguiri@chromaway.com>
2025-11-26 16:25:41 +0000

    chore(dependencies): downgrade Rell 0.14.16 related dependency versions
    ## What does this Solve?
    
    ## Release Notes
    
    ### Bugfix
    
    - downgrade Rell version to `0.14.15` to mitigate a Rell bug

--- chromia-cli/src/test/kotlin/com/chromia/cli/command/FetchConfigCommandTest.kt
+++ chromia-cli/src/test/kotlin/com/chromia/cli/command/FetchConfigCommandTest.kt
@@ -130,7 +130,7 @@ class FetchConfigCommandTest : IntegrationTestSetup() {
       - net.postchain.web.WebStaticGTXModuleFactory
       - net.postchain.gtx.StandardOpsGTXModule
       rell:
-        compilerVersion: 0.14.16
+        compilerVersion: 0.14.15
         moduleArgs:
           main:
             foo: bar
